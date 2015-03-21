ed8f2e37db636a781ab07eaf8b125e598a509307
Julius Susanto <julius.susanto@gmail.com>
2015-03-21 14:01:01 +1100

    Added version information

---
+++ pydyn/version.py
@@ -0,0 +1,34 @@
+#!python3
+#
+# Copyright (C) 2014-2015 Julius Susanto
+#
+# PYPOWER-Dynamics is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# PYPOWER-Dynamics is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PYPOWER-Dynamics. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+PYPOWER-Dynamics
+Version Information
+
+"""
+
+def pydyn_ver(*args):
+    """ 
+    Returns PYPOWER-Dynamics version info for current installation.
+    """
+
+    ver = {'Name': 'PYPOWER-Dynamics',
+           'Version': '1.0.1',
+           'Release':  '',
+           'Date': '14-Mar-2015'}
+
+    return ver
